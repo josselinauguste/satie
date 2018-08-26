bd99de98afbc6d75ae8a6c66baf6e83f53e2fb29
Josselin Auguste <jauguste@iblop.net>
2018-08-26 20:41:00 +0200

    Generate music using markov chain

--- src/Composer.hs
+++ src/Composer.hs
@@ -4,9 +4,42 @@ module Composer
 where
 
 import           Euterpea
+import           System.Random                  ( random
+                                                , StdGen
+                                                , newStdGen
+                                                )
+
+import           Markov                         ( TransitionMatrix
+                                                , next
+                                                )
 
 playNewComposition :: IO ()
-playNewComposition = play generateComposition
+playNewComposition = do
+    gen <- newStdGen
+    play $ generateComposition gen
+    return ()
 
-generateComposition :: Music Pitch
-generateComposition = d 3 en
+generateComposition :: StdGen -> Music Pitch
+generateComposition gen =
+    line $ take duration $ map (note sn . (\n -> (n, 3)) . fst) $ iterate
+        generateNextPitch
+        (C, gen)
+  where
+    duration = 16
+    generateNextPitch :: (PitchClass, StdGen) -> (PitchClass, StdGen)
+    generateNextPitch (fromPitch, currentGen) =
+        let (choice, nextGen) = random currentGen :: (Float, StdGen)
+            nextPitch         = next matrix fromPitch choice
+        in  case nextPitch of
+                Just actualPitch -> (actualPitch, nextGen)
+                Nothing          -> (C, nextGen)
+
+matrix :: TransitionMatrix PitchClass
+matrix =
+    [ (C, [(C, 0.33), (G, 0.67)])
+    , (D, [(C, 0.5), (D, 0.25), (G, 0.25)])
+    , (E, [(D, 0.375), (E, 0.625)])
+    , (F, [(E, 0.5), (F, 0.5)])
+    , (G, [(F, 0.6), (G, 0.4)])
+    , (A, [(G, 0.5), (A, 0.5)])
+    ]
