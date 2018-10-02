65f6bb0917bdfc019f51cd114d3dc115ea8ae930
Josselin Auguste <jauguste@iblop.net>
2018-10-02 21:18:13 +0200

    Remove magic pitches in markov chain composition -- they ended up creating infinite chain of the same pitch

--- src/Composer.hs
+++ src/Composer.hs
@@ -21,12 +21,14 @@ generateComposition :: Algorithm -> Music Pitch
 generateComposition (Random gen) =
     line $ map (note sn . pitch) $ randomRs (30, 80) gen
 generateComposition (MarkovChain (matrix, gen)) =
-    line $ map (note sn . fst) $ iterate generateNextPitch ((C, 3), gen)
+    line $ map (note sn . fst) $ iterate (generateNextPitch firstPitch)
+                                         (firstPitch, gen)
   where
-    generateNextPitch :: (Pitch, StdGen) -> (Pitch, StdGen)
-    generateNextPitch (fromPitch, currentGen) =
+    firstPitch = fst $ head matrix
+    generateNextPitch :: Pitch -> (Pitch, StdGen) -> (Pitch, StdGen)
+    generateNextPitch defaultPitch (fromPitch, currentGen) =
         let (choice, nextGen) = random currentGen :: (Float, StdGen)
             nextPitch         = next matrix fromPitch choice
         in  case nextPitch of
                 Just actualPitch -> (actualPitch, nextGen)
-                Nothing          -> ((C, 1), nextGen)
+                Nothing          -> (defaultPitch, nextGen)
--- test/Spec.hs
+++ test/Spec.hs
@@ -46,11 +46,20 @@ main = hspec $ do
       $             lineToList (generateComposition $ Random (mkStdGen 0))
       `shouldNotBe` []
     describe "using markov chain" $ do
-      let matrix = [((C, 3), [((C, 3), 0.33), ((G, 3), 0.67)])]
+      let matrix = [((E, 3), [((C, 3), 0.33), ((G, 3), 0.67)])]
 
-      it "composes a song starting with C"
+      it "composes a song starting with first note"
         $ lineToList (generateComposition $ MarkovChain (matrix, mkStdGen 0))
-        `shouldStartWith` [c 3 sn]
+        `shouldStartWith` [e 3 sn]
+
+      it "fills non resolved pitches with first pitch"
+        $                 do
+                            let nonResolvingMatrix = [((E, 3), [((A, 3), 1)])]
+                            lineToList
+                              ( generateComposition
+                              $ MarkovChain (nonResolvingMatrix, mkStdGen 0)
+                              )
+        `shouldStartWith` [e 3 sn, a 3 sn, e 3 sn]
 
   describe "parse song" $ do
     it "parses a minimal MusicXML document"
